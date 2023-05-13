e229b3a8ed1bb65641152bfefae35200d896107f
aaravdit <133431343+aaravdit@users.noreply.github.com>
2023-05-13 13:23:02 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>Font Awesome Icons</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+<!--Get your own code at fontawesome.com-->
+
+</head>
+
+<body>
+
+<i class="fas fa-clock fa-5x"></i>
+
+<!--fa-ul and fa-li classes are used to replace default bullets in unordered lists-->
+
+<ul class="fa-ul">
+
+  <li><span class="fa-li"><i class="fas fa-check-square"></i></span>List Item</li>
+
+  <li><span class="fa-li"><i class="fas fa-spinner fa-pulse"></i></span>List Item</li>
+
+  <li><span class="fa-li"><i class="fas fa-square"></i></span>List Item</li>
+
+</ul>
+
+<!--icons animation; fa-spin class gets any icon to rotate, and the fa-pulse class gets any icon to rotate-->
+
+<i class="fas fa-circle-notch fa-spin"></i>
+
+<i class="fas fa-sync-alt fa-spin"></i>
+
+<i class="fas fa-cog fa-spin"></i>
+
+<i class="fas fa-cog fa-pulse"></i>
+
+<br>
+
+<!--fa-rotate-* and fa-flip-* classes are used to rotate and flip icons-->
+
+<i class="fas fa-horse fa-rotate-90"></i>
+
+<i class="fas fa-horse fa-rotate-180"></i>
+
+<i class="fas fa-horse fa-rotate-270"></i>
+
+ 
+
+</body>
+
+</html>
